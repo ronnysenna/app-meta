f61343382c8dfa12b3e9e373f8c06c9440052424
Ronny Senna <ronnysenna@yahoo.com>
2024-09-09 22:10:05 -0300

    add evolução

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let mensagem = "Deus no comando"
-const num = 10
-console.log(mensagem);
-console.log(num);
+// let mensagem = "Deus no comando"
+// const num = 10
+// console.log(mensagem);
+// console.log(num);
+
+// let metas = ["Ronny","Senna"]
+// console.log(metas[0]  + " " +  metas[1])
+
+let meta = {
+    value : "Ler um livro",
+    Checked: false
+}
+
+console.log(meta.value)
